5badab55b7b6759987005e2b6d6b9702db068447
Sotatek-HieuVu <hieu.vu@sotatek.com>
2022-04-15 02:01:05 +0700

    xong bai 8

--- app/src/main/java/com/example/democreateproject/MainActivity.java
+++ app/src/main/java/com/example/democreateproject/MainActivity.java
@@ -7,51 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final String TAG = "CNTT-K65";
+        final String TAG = "hieuvh";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnTest = findViewById(R.id.btn_test);
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent testIntent = new Intent(MainActivity.this, ScrollingActivity.class);
-                testIntent.putExtra("test", "test put data in intent");
-
-                Log.d(TAG, "onClick: test log");
-                startActivity(testIntent);
-            }
-        });
+        imageView = findViewById(R.id.anh_cua_ngan);
+        imageView.setOnClickListener(view -> onClickImage());
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    private void onClickImage(){
+        Toast.makeText(this, "Ban vua click vao anh", Toast.LENGTH_LONG).show();
     }
 }
